df6f4ed9db374f6637868c4b921a75097be4d78a
pipavlo82 <shtomko@gmail.com>
2025-10-12 10:03:58 -0400

    Update r4cs.h

--- r4cs.h
+++ r4cs.h
@@ -1,24 +1,22 @@
+// r4cs.h
 #pragma once
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Ініціалізація DRBG.
-// r4_path: шлях до re4_stream (або NULL => береться з $R4_PATH або "re4_stream").
-// Повертає 0 при успіху, <0 при помилці.
-int r4cs_init(const char *r4_path);
+typedef struct r4ctx {
+    unsigned char key[32];
+    unsigned char nonce[12];
+    uint32_t      counter;
+} r4ctx;
 
-// Згенерувати n байт у out.
-// Повертає 0 при успіху, <0 при помилці.
-int r4cs_random(void *out, size_t n);
-
-// Форсований reseed (опціонально).
-int r4cs_reseed(void);
-
-// Закрити пайп та занулити ключі.
-void r4cs_close(void);
+int  r4_init_from_system(r4ctx *ctx);
+int  r4_init_from_path(r4ctx *ctx, const char *path);
+void r4_free(r4ctx *ctx);
+int  r4_generate(r4ctx *ctx, unsigned char *out, size_t out_len);
 
 #ifdef __cplusplus
 }
